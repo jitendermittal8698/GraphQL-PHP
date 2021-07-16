cd370fc02e8f3ebd99c6d7384672b4eb1770ad72
jitendermittal8698 <47344233+jitendermittal8698@users.noreply.github.com>
2021-07-16 09:45:25 +0530

    Create EntryPoints.php

---
+++ Myapp/EntryPoint/EntryPoints.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * @file
+ */
+
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+
+$queryType = new ObjectType([
+  'name' => 'Query',
+  'fields' => [
+    'hello' => [
+      'type' => Type::string(),
+      'resolve' => static function ($rootValue, array $args) {
+        return 'Hello World!!!';
+      },
+    ],
+    'echo' => [
+      'type' => Type::string(),
+      'args' => [
+        'message' => ['type' => Type::string()],
+      ],
+      'resolve' => static function ($rootValue, array $args) {
+        return $rootValue['prefix'] . $args['message'];
+      },
+    ],
+    'authors' => [
+      'type' => Type::listOf($author),
+      'args' => [
+        'id' => ['type' => Type::int()],
+      ],
+      'resolve' => static function ($rootValue, array $args) {
+        if (isset($args['id'])) {
+          return DataSource::findAuthor($args['id']);
+        }
+        else {
+          return DataSource::getAuthors();
+        }
+
+      },
+    ],
+    'books' => [
+      'type' => Type::listOf($book),
+      'args' => [
+        'isbn' => ['type' => Type::string()],
+      ],
+      'resolve' => static function ($rootValue, array $args) {
+        if (isset($args['isbn'])) {
+          return DataSource::findBook($args['isbn']);
+        }
+        else {
+          return DataSource::getBooks();
+        }
+      },
+    ],
+  ],
+]);
+
+$mutationType = new ObjectType([
+  'name' => 'Mutation',
+  'fields' => [
+    'updateAuthor' => [
+      'type' => $author,
+      'args' => [
+        'id' => ['type' => Type::string()],
+        'data' => ['type' => $authorInput],
+      ],
+      'resolve' => static function ($calc, array $args) {
+        return DataSource::updateAuthor($args['id'], $args['data']);
+      },
+    ],
+    'createAuthor' => [
+      'type' => $author,
+      'args' => [
+        'id' => ['type' => Type::string()],
+        'data' => ['type' => $authorInput],
+      ],
+      'resolve' => static function ($calc, array $args) {
+        return DataSource::createAuthor($args['id'], $args['data']);
+      },
+    ],
+    'authors' => [
+      'type' => Type::listOf($author),
+      'args' => [
+        'id' => ['type' => Type::int()],
+      ],
+      'resolve' => static function ($rootValue, array $args) {
+        if (isset($args['id'])) {
+          return DataSource::findAuthor($args['id']);
+        }
+        else {
+          return DataSource::getAuthors();
+        }
+      },
+    ],
+  ],
+]);
